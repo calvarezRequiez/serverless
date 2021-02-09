384fe99552d54d3ba846ce073fc6b778a4da131c
Carlos Alvarez <car.alv2804@gmail.com>
2021-02-09 16:35:52 -0600

    |

--- api/models/Meals.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const Meals = mongoose.model('Meal', new Schema({
-    name: String, 
-    desc: String,
-}))
-
-module.exports = Meals
