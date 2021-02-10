7db4e315ba196f85f1fc3394eda609d8eaffb5df
Carlos Alvarez <car.alv2804@gmail.com>
2021-02-09 19:45:56 -0600

    Trayendo los datos del servidor para mostrarlos en la app

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 window.onload = () => {
-    fetch('')
+    fetch('https://serverless.carlosalvarez.vercel.app/api/meals')
+    // Convirtiendo el arreglo a JSON
+    .then(response => response.json())
+    // Mostrando la data 
+    .then(data => {
+        const mealsList = document.getElementById('meals-list')
+        const submit = document.getElementById('submit')
+        const template = data.map(x => '<li>' + x.name + '</li>').join('')
+        mealsList.innerHTML = template
+        submit.removeAttribute('disabled')
+    })
 }
